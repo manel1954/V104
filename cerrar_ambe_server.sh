7c7dd9998c680afabe1a6038946df6fdb038d075
Manel <ea3eiz@ea3eiz.com>
2020-02-07 16:57:43 +0100

    Update cerrar_ambe_server.sh

--- cerrar_ambe_server.sh
+++ cerrar_ambe_server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp abrir_ambe_server.desktop /home/pi
+sudo cp Abrir_ambe_server.desktop /home/pi
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ambe_server.sh'" /home/pi/abrir_ambe_server.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/abrir_ambe_server.desktop
-sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/abrir_ambe_server.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Abrir_ambe_server.desktop
+sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi
-sudo cp abrir_ambe_server.desktop /home/pi/Desktop
+sudo cp Abrir_ambe_server.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/abrir_ambe_server.desktop
+sudo rm /home/pi/Abrir_ambe_server.desktop
 
 sudo killall AMBEserver
 
