ef005b62b67b8bf6d10ce9cde03e79bb77650add
Manel <ea3eiz@ea3eiz.com>
2020-02-18 20:24:25 +0100

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -34,12 +34,16 @@ sudo rm /home/pi/RXF_DMRPLUS.desktop
 sed -i "2c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-
-sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_DMRPLUS.sh'" /home/pi/Desktop/Abrir_MMDVMPLUS.desktop
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Desktop/Abrir_MMDVMPLUS.desktop
-sudo sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Desktop/Abrir_MMDVMPLUS.desktop
+cd /home/pi/Desktop
+sudo cp Abrir_MMDVMPLUS.desktop /home/pi
+sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sudo sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
-
+cd /home/pi
+sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
 
 cd /home/pi/MMDVMHost
 echo "\33[1;37m"
