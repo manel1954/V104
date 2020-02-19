24f57baf555413bbd59cd92f36ef023efa7322e1
Manel <ea3eiz@ea3eiz.com>
2020-02-19 11:56:34 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -23,14 +23,14 @@ sed -i "17c NXDN=OFF" /home/pi/status.ini
 
 #Actualiza todos los iconos y Quita todos los iconos verdes que se quedan al cerrar la imagen
 
-cp /home/pi/Desktop/Activar_dvswitch.desktop /home/pi #deja el icono en el estado que se reinició
+#cp /home/pi/Desktop/Activar_dvswitch.desktop /home/pi #deja el icono en el estado que se reinició
 
 
 cd /home/pi/$SCRIPTS_version/Desktop
 cp * /home/pi/Desktop
 sleep 1
 
-cp /home/pi/Activar_dvswitch.desktop /home/pi/Desktop #deja el icono en el estado que se reinició
+#cp /home/pi/Activar_dvswitch.desktop /home/pi/Desktop #deja el icono en el estado que se reinició
 
 
 #Actualiza Imagen
