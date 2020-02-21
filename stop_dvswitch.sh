16c50aab6773dec67e6f2d666ae93b7467503d90
Manel <ea3eiz@ea3eiz.com>
2020-02-21 23:07:55 +0100

    Update stop_dvswitch.sh

--- stop_dvswitch.sh
+++ stop_dvswitch.sh
@@ -17,11 +17,13 @@ sleep 1
 sudo rm /home/pi/Abrir_ircDDBGateway.desktop
 
 
+
+
 clear
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Activar_dvswitch.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_dvswitch.sh'" /home/pi/Activar_dvswitch.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DVSWITCH_OFF.png" /home/pi/Activar_dvswitch.desktop
 sed -i "10c Name[es_ES]=Activar Dvswitch" /home/pi/Activar_dvswitch.desktop
 
