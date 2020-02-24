c65f6f2772c28471de722c135fe032e6e50ad02b
Manel <ea3eiz@ea3eiz.com>
2020-02-24 17:28:42 +0100

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -5,18 +5,12 @@ sudo systemctl restart ircddbgateway
 sudo systemctl restart mmdvm_bridge
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-sudo cp Abrir_solodstar.desktop /home/pi
-sleep 1
-sed -i "6c Exec=sh -c 'cd /home/pi/V104/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICONO_DVSWITCH_OFF" /home/pi/Abrir_solodstar.desktop
-sed -i "11c Name[es_ES]=AbrirXX solo D-STAR" /home/pi/Abrir_solodstar.desktop
+
+sed -i "6c Exec=sh -c 'cd /home/pi/V104/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solodstar.sh'" /home/pi/DesktopAbrir_solodstar.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICONO_DVSWITCH_OFF" /home/pi/DesktopAbrir_solodstar.desktop
+sed -i "11c Name[es_ES]=AbrirXX solo D-STAR" /home/pi/DesktopAbrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
-sleep 1
-cd /home/pi
-sudo cp Abrir_solodstar.desktop /home/pi/Desktop
-sleep 1
-sudo rm /home/pi/Abrir_solodstar.desktop
+
 
 sudo killall MMDVMDSTAR
 sudo killall ircddbgateway
