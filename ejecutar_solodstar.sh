57ad85bfd5995355497a52a296e8a44d9f86e101
Manel <ea3eiz@ea3eiz.com>
2020-02-24 17:32:57 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -66,7 +66,7 @@ sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 # Cierra el icono Abrir Solo Dstar si no hay conecta
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=ON" /home/pi/status.ini
