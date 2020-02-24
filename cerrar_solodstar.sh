48340006927830db4a5bc1c62c3bbf2ac7b3ad46
Manel <ea3eiz@ea3eiz.com>
2020-02-24 17:43:33 +0100

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -10,7 +10,7 @@ sudo cp Abrir_solodstar.desktop /home/pi
 sleep 1
 
 sed -i "6c Exec=sh -c 'cd /home/pi/V104/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICONO_DVSWITCH_OFF" /home/pi/Abrir_solodstar.desktop
+sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
 sleep 1
