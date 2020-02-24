ed0f6a40b50b30f8ee77fd097e169b5e8ee66229
Manel <ea3eiz@ea3eiz.com>
2020-02-24 13:19:12 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -16,6 +16,7 @@ echo "******************************************"
 sleep 5
 clear
 
+#Escribe en el fichero INFO_RXF para poner los datos del icono INFO TXF
 mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDSTAR.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
@@ -33,10 +34,9 @@ sed -i "11c Name=$frecuencia" /home/pi/RXF_DSTAR.desktop
 sleep 1
 sudo cp /home/pi/RXF_DSTAR.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/pi/RXF_DSTAR.desktop
-
-#Escribe en el fichero INFO_RXF para poner los datos del icono INFO TXF                         
+sudo rm /home/pi/RXF_DSTAR.desktop                       
 sed -i "5c $frecuencia" /home/pi/INFO_RXF
+# FIN Escribe en el fichero INFO_RXF para poner los datos del icono INFO TXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
@@ -64,7 +64,7 @@ echo "***********************************************"
 sleep 1
 sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 
-
+# Cierra el icono Abrir Solo Dstar si no hay conecta
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
 sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
