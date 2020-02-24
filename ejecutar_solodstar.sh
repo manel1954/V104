f7bf88b6a0cbc4b228e0e17bc2bd7470833d3696
Manel <ea3eiz@ea3eiz.com>
2020-02-24 13:16:41 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-sudo systemctl stop ircddbgateway.service
-modo=$(awk "NR==18" /home/pi/status.ini)
-if [ "$modo" = 'DVSWITCH=ON' ];then
-echo "\033[1;31m" #ROJO
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+
+echo "${ROJO}"
 echo "******************************************"
 echo "*        ESTO DESACTIVARÁ D-STAR         *"
 echo "*              EN DVSWITCH               *"
 echo "******************************************"
 sleep 5
-#else
 clear
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-echo "***********************************************"
-echo "*             ABRIENDO SOLO D-STAR            *"
-echo "***********************************************"
+
 mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMDSTAR.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
@@ -48,6 +50,7 @@ sudo cp Abrir_solodstar.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_solodstar.desktop
 
+# Pone Enable=0 de [Dstar Network]
 sed -i "62c Enable=0" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
 sudo systemctl stop ircddbgateway
 sudo systemctl restart mmdvm_bridge.service
@@ -61,20 +64,7 @@ echo "***********************************************"
 sleep 1
 sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 
-else
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-sudo cp Abrir_solodstar.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh cerrar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR_ON.png" /home/pi/Abrir_solodstar.desktop
-sed -i "11c Name[es_ES]=Cerrar solo D-STAR" /home/pi/Abrir_solodstar.desktop
-sed -i "13c SOLODSTAR=ON" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solodstar.desktop /home/pi/Desktop
-sleep 1
-sudo rm /home/pi/Abrir_solodstar.desktop
 
-fi
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
 sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
