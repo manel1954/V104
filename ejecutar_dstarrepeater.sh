ccb4b1a29081987bbc15c8aa4d2e949a5c89ec44
Manel <ea3eiz@ea3eiz.com>
2020-02-28 17:25:19 +0100

    Update ejecutar_dstarrepeater.sh

--- ejecutar_dstarrepeater.sh
+++ ejecutar_dstarrepeater.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-echo "\033[1;31m" #ROJO
-echo "******************************************"
-echo "      NO SE PUEDE ABRIR ESTE SISTEMA     *"
-echo "       SI ESTA EL DVSWITCH ACTIVADO      *"
-echo "******************************************"
-sleep 5
-
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+echo "${ROJO}"
+echo "                  ******************************************"
+echo "                  *        ESTO DESACTIVARÁ D-STAR         *"
+echo "                  *              EN DVSWITCH               *"
+echo "                  ******************************************"
+sleep 3
+clear
+echo "${VERDE}"
+clear
+echo "                  ***********************************************"
+echo "                  *            ABRIENDO ircDDBGateway           * "
+echo "                  ***********************************************"
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDB.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_ircDDB.sh'" /home/pi/Abrir_ircDDB.desktop
