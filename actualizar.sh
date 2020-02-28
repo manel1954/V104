09f19a677e34f8bf526c29cfd63893caa8796489
Manel <ea3eiz@ea3eiz.com>
2020-02-28 09:34:12 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -4,6 +4,10 @@ sleep 10
 usuario="/home/pi"
 usuario="$usuario"
 SCRIPTS_version="V104"
+actualizacion=$(awk "NR==1" /home/pi/.config/autostart/actualizacion)
+SCRIPTS_version="V10"
+version="V104-"
+version=$version$actualizacion
 #pone todos los status de inicio en OFF
 sed -i "1c D-STAR=OFF" $usuario/status.ini
 sed -i "2c BlueDV=OFF" $usuario/status.ini
