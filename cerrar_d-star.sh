018b7183400e4793c413ff0fb6f55c08fd7ffb0c
Manel <ea3eiz@ea3eiz.com>
2020-02-28 12:51:27 +0100

    Update cerrar_d-star.sh

--- cerrar_d-star.sh
+++ cerrar_d-star.sh
@@ -15,7 +15,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal -e sudo sh ejecutar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
