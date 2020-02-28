0340ca7e7d2293db6f63593600cdeeac7c3c4b5c
Manel <ea3eiz@ea3eiz.com>
2020-02-28 09:10:09 +0100

    Update ejecutar_DMR2NXDN.sh

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -51,4 +51,16 @@ sudo lxterminal --geometry=80x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
 
 cd /home/pi/NXDNClients/NXDNGateway
 sudo ./NXDNGateway NXDNGateway.ini
+
+cd /home/pi/Desktop
+sudo cp Abrir_DMR2NXDN.desktop /home/pi
+sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_DMRPLUS.sh'" /home/pi/Abrir_DMR2NXDN.desktop
+sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_DMR2NXDN.desktop
+sudo sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_DMR2NXDN.desktop
+sudo sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
+cd /home/pi
+sudo cp Abrir_DMR2NXDN.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_DMR2NXDN.desktop
+
 fi
