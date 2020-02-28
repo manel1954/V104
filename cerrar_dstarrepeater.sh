d6f40e392037fad233f6f7707b07de41b3adaa7e
Manel <ea3eiz@ea3eiz.com>
2020-02-28 18:32:35 +0100

    Update cerrar_dstarrepeater.sh

--- cerrar_dstarrepeater.sh
+++ cerrar_dstarrepeater.sh
@@ -5,7 +5,8 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_dstarrepeater.desktop /home/pi
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dstarrepeater.sh'" /home/pi/Abrir_dstarrepeater.desktop
+
+sed -i "4c Exec=sh -c 'cd /home/pi/V104; lxterminal -e sudo sh ejecutar_dstarrepeater.sh; sudo dstarrepeater & sudo ircddbgateway -gui'" /home/pi/Abrir_dstarrepeater.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/repeater.png" /home/pi/Abrir_dstarrepeater.desktop
 sed -i "10c Name[es_ES]=Abrir D-STAR Repeater" /home/pi/Abrir_dstarrepeater.desktop
 sed -i "9c dstarrepeater=OFF" /home/pi/status.ini
@@ -17,7 +18,8 @@ sudo rm /home/pi/Abrir_dstarrepeater.desktop
 #cierra ircDDBGateway y quita icono verde de "cerrar ircDDB"
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDB.desktop /home/pi
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_d-star.sh'" /home/pi/Abrir_ircDDB.desktop
+
+sed -i "4cExec=sh -c 'cd /home/pi/V104; lxterminal -e sudo sh ejecutar_ircDDB.sh; sudo ircddbgateway -gui'" /home/pi/Abrir_ircDDB.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDB.desktop
 sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDB.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
