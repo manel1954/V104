72180ea38b87849eda03563132ce168174e859e9
Manel <ea3eiz@ea3eiz.com>
2020-02-28 20:28:34 +0100

    Update borrar_logs.sh

--- borrar_logs.sh
+++ borrar_logs.sh
@@ -9,8 +9,11 @@ clear
 sudo rm /home/pi/DMR2YSF/*.log
 clear
 sudo rm /home/pi/NXDNClients/NXDNGateway/*.log
-sudo rm /var/log/irc*
-sudo rm /var/log/MMDVM*
+
+sudo rm /var/log/*.log
+sudo rm /var/log/dvswitch/Analog*
+
+sudo rm /var/log/mmdvm/MMDVM*
 clear
 echo "\33[1;32m<<<<< BORRANDO TODOS LOS LOGS >>>>>"
 sleep 5
