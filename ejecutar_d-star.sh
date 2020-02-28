ac98436e288e689c61ac9496c9af8f14b59dd64b
Manel <ea3eiz@ea3eiz.com>
2020-02-28 10:35:55 +0100

    Update ejecutar_d-star.sh

--- ejecutar_d-star.sh
+++ ejecutar_d-star.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-
-
-echo "******************************************"
-echo "*        ESTO DESACTIVARÁ D-STAR         *"
-echo "*              EN DVSWITCH               *"
-echo "******************************************"
-sleep 3
-
-clear
-echo "***********************************************"
-echo "*             ABRIENDO D-STAR                 *"
-echo "***********************************************"
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+echo "${ROJO}"
+echo "                  ******************************************"
+echo "                  *        ESTO DESACTIVARÁ D-STAR         *"
+echo "                  *              EN DVSWITCH               *"
+echo "                  ******************************************"
+sleep 3
+clear
+echo "${VERDE}"
+clear
+echo "                  ***********************************************"
+echo "                  *             ABRIENDO SOLO D-STAR            * "
+echo "                  ***********************************************"
+
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
