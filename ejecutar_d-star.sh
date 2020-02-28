e4d92d02ed50538f81bfcf6741dab994001be297
Manel <ea3eiz@ea3eiz.com>
2020-02-28 13:46:46 +0100

    1

--- ejecutar_d-star.sh
+++ ejecutar_d-star.sh
@@ -36,7 +36,7 @@ sed -i "62c Enable=0" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
 sudo systemctl stop ircddbgateway
 sudo systemctl restart mmdvm_bridge.service
 
-sudo ircddbgateway -gui
+sudo ircddbgateway -gui &
 
 
 
