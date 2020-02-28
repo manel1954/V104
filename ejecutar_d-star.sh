75f97fc9a71f3249c5e8b97b1c37e14e121a7311
Manel <ea3eiz@ea3eiz.com>
2020-02-28 10:33:30 +0100

    1

--- ejecutar_d-star.sh
+++ ejecutar_d-star.sh
@@ -15,7 +15,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=ON" /home/pi/status.ini
 cd /home/pi
@@ -33,7 +33,7 @@ sudo ircddbgateway -gui
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR_OFF.png" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
 cd /home/pi
