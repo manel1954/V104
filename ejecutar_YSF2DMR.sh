e2b6b49aa28616b433161f4347fc069ab20ec2a8
Manel <ea3eiz@ea3eiz.com>
2020-02-28 09:23:49 +0100

    Update ejecutar_YSF2DMR.sh

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -52,7 +52,7 @@ sudo ./MMDVMFUSION MMDVMFUSION.ini
 cd /home/pi/Desktop
 sudo cp Abrir_YSF2DMR.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x20 -e sudo sh ejecutar_YSF2DMR.sh'" /home/pi/Abrir_YSF2DMR.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_YSF2DMR_ON.png" /home/pi/Abrir_YSF2DMR.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_YSF2DMR_OFF.png" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "10c Name[es_ES]=Abrir YSF2DMR" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "14c YSF2DMR=ON" /home/pi/status.ini
 cd /home/pi
