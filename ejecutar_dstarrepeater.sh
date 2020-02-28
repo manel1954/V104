63681d70339c018cc630ca3de816fb6e9a322c56
Manel <ea3eiz@ea3eiz.com>
2020-02-28 13:14:30 +0100

    Update ejecutar_dstarrepeater.sh

--- ejecutar_dstarrepeater.sh
+++ ejecutar_dstarrepeater.sh
@@ -23,7 +23,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDBGateway.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_d-star.sh'" /home/pi/Abrir_ircDDBGateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/DSTAR_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_ON.png" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Abrir_ircDDBGateway.desktop
 sed -i "1c D-STAR=ON" /home/pi/status.ini
 cd /home/pi
