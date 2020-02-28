8fbfa8d74d7a80f3f23b6a649802b3113a62f82e
Manel <ea3eiz@ea3eiz.com>
2020-02-28 09:46:39 +0100

    1

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -3,7 +3,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ECHOLINK_ON.png" /home/pi/Abrir_svxlink.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_ON.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Cerrar SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=ON" /home/pi/status.ini
 cd /home/pi
@@ -16,7 +16,7 @@ sudo svxlink
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ECHOLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
 cd /home/pi
