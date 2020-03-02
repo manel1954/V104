913256143da66daff718cef4d48670eb8dba458d
Manel <ea3eiz@ea3eiz.com>
2020-03-02 15:40:53 +0100

    Update Actualizar_dvswitch.sh

--- Actualizar_dvswitch.sh
+++ Actualizar_dvswitch.sh
@@ -11,32 +11,29 @@ BLANCO="\033[1;37m"
 AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
-
+clear
+echo "${VERDE}"
+echo " *********************************************************************"
+echo " *                                                                   *"
+echo " *                      ACTUALIZAR DVSWITCH                          *"
+echo " *                                                                   *"
+echo " *********************************************************************"
 echo "${AMARILLO}"
 echo -n " Quieres Actualizar Dvswitch S/N ? "
 read activar
 case "$activar" in
 [sS]* )
-clear
-echo "${VERDE}"
-echo " *********************************************************************"
-echo " *                                                                   *"
-echo " * <<<<<<<<<<<<<<<<<< ACTUALIZANDO DVSWITCH   >>>>>>>>>>>>>>>>>>>>>> *"
-echo " *                                                                   *"
-echo " *********************************************************************"
-sleep 3
+
 version=$(awk "NR==11" $usuario/info.txt)
 version=`expr substr $version 18 5`
 echo "$version"
-read a
-
 if [ $version = 6.0.2 ]
 then
-	echo "la versión no ha cambiado"
-	read a
+	echo "Ya tienes la última versión"
+	sleep 3
 else
-	echo "la version ha cambiado"
-	read a
+	echo "Hay una nueva versión y se está actualizando"
+	sleep 3
 fi
 break;;
 [nN]*) 
