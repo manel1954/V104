c2cfc8b2479ede9a627b4b2df22f0978b72f205b
Manel <ea3eiz@ea3eiz.com>
2020-03-05 09:53:47 +0100

    Update Actualizar_dv4mini.sh

--- Actualizar_dv4mini.sh
+++ Actualizar_dv4mini.sh
@@ -3,7 +3,7 @@ clear
 while true
 do
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-#Colores
+#Colores 
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
 BLANCO="\033[1;37m"
@@ -20,6 +20,7 @@ echo  "\33[1;36m   1)\33[1;33m Actualizar \33[1;32mDV4MINI20190307   \33[1;33m"
 echo  "\33[1;36m   2)\33[1;33m Volver a la versión \33[1;32mDV4MINI20190127   \33[1;33m"
 echo  "\33[1;36m   3)\33[1;33m Volver a la versión \33[1;32mDV4MINI20170517   \33[1;33m"
 echo  "\33[1;36m   4)\33[1;33m Actualizar \33[1;32mDV4MINI20190307${AMARILLO} (NEW)   \33[1;33m"
+echo  "\33[1;36m   5)\33[1;33m Actualizar \33[1;32mDV4MINI-RPI_2020_03_03${AMARILLO} (NEW)   \33[1;33m"
 echo ""
 echo -n "\33[1;36m   Elige una opción: " 
 read escoger_menu
@@ -145,6 +146,41 @@ clear
                         sudo mkdir /home/pi/dv4mini
                         sudo chmod 777 -R /home/pi/dv4mini
                         cd /home/pi/$CRIPTS_version/DV4MINI/20190307new
+                        cp dv_serial /home/pi/dv4mini
+                        cp dv4mini.exe /home/pi/dv4mini
+                        cp xref.ip /home/pi/dv4mini
+                        sudo cp dv_serial /usr/bin/
+                        cd /usr/bin/
+                        sudo chmod 777 dv_serial
+                        cd /home/pi/dv4mini
+                        sudo chmod 777 dv_serial
+                        clear
+                        echo "************************************"
+                        echo "*** SE HA ACTUALIZADO CON EXITO  ***"
+                        echo "************************************"
+                        sleep 3                        
+                        clear
+                        break;;
+                        [nN]* ) echo ""
+                        break;;
+esac
+done;;
+5) echo ""
+while true
+do
+clear
+                        ejecutar1=S
+                        case $ejecutar1 in
+                        [sS]* ) echo ""
+                        echo ">>>>>>>>> ACTUALIZANDO >>>>>>>>"
+                        sudo rm -r /home/pi/$CRIPTS_version/DV4MINI/
+                        cd /home/pi/$CRIPTS_version
+                        git clone http://github.com/ea3eiz/DV4MINI
+                        sudo rm -r /home/pi/dv4mini
+                        sudo rm /usr/bin/dv_serial
+                        sudo mkdir /home/pi/dv4mini
+                        sudo chmod 777 -R /home/pi/dv4mini
+                        cd /home/pi/$CRIPTS_version/DV4MINI/DV4MINI-RPI_2020_03_03
                         cp dv_serial /home/pi/dv4mini
                         cp dv4mini.exe /home/pi/dv4mini
                         cp xref.ip /home/pi/dv4mini
