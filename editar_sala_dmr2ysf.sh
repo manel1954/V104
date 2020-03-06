74f0baccaa797ad81858d7d70b6a76bdc88624d8
Manel <ea3eiz@ea3eiz.com>
2020-03-06 21:56:03 +0100

    Update editar_sala_dmr2ysf.sh

--- editar_sala_dmr2ysf.sh
+++ editar_sala_dmr2ysf.sh
@@ -54,6 +54,11 @@ do
 			                    [sS]* ) echo ""
 
 sala=`grep -n "ES ES ALMERIA" /home/pi/YSFClients/YSFGateway/YSFHosts.txt`
+buscar=":"
+largo_linea=`expr index $sala $buscar` #comprueba el largo incluyendo los dos puntos (:)
+largo_linea=`expr $largo_linea - 1` #comprueba el largo quitando los dos puntos (:)
+numero_linea=`expr substr $sala 1 $largo_linea` # recoge el numero de linea ejemplo (74)
+numero_linea_fusion=`expr $numero_linea - 1` # y le resta uno quedando como: ejemplo (73)
 echo "pausa $sala"
 read a
 
