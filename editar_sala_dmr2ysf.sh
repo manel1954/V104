f8661200df1c2312131a563c5ae6d130dc4f991f
Manel <ea3eiz@ea3eiz.com>
2020-03-07 14:02:40 +0100

    Update editar_sala_dmr2ysf.sh

--- editar_sala_dmr2ysf.sh
+++ editar_sala_dmr2ysf.sh
@@ -36,7 +36,10 @@ SALA3=$(awk "NR==3" /home/pi/nombre_salas_ysf.txt)
 echo "TG: $TG3  SALA: $SALA3"
 
 
-
+echo ""
+echo "${BLANCO}"
+echo "   11) Abrir listado de salas YSF para ver su nombre"
+echo "   12) Cerrar listado de salas YSF"
 echo ""
 echo "   ${ROJO}0) Salir ${AMARILLO}(si usas ratón puedes salir directamente con la x del terminal)"
 echo ""
@@ -102,6 +105,19 @@ do
                           sudo sed -i "7c $listsala" /home/pi/DMR2YSF/TG-YSFList.txt
                           sudo sed -i "3c $tg" /home/pi/tg_ysf.txt
                           sudo sed -i "3c $NOMBRE_SALA" /home/pi/nombre_salas_ysf.txt
+                          break;;
+                          [nN]* ) echo ""
+                          break;;
+esac
+done;;
+11) echo ""
+while true
+do
+                        
+                          actualizar=S 
+                          case $actualizar in
+                          [sS]* ) echo ""
+                          sudo geany /home/pi/DMR2YSF/TG-YSFList.txt
                           break;;
                           [nN]* ) echo ""
                           break;;
