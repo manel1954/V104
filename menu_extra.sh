ea65fd74d3d144bd58f4c5b22766c8e36135e28d
Manel <ea3eiz@ea3eiz.com>
2020-03-10 23:02:51 +0100

    Update menu_extra.sh

--- menu_extra.sh
+++ menu_extra.sh
@@ -46,24 +46,6 @@ echo -n "\33[1;36m   Elige una opción: "
 read escoger_menu
 echo ""
 case $escoger_menu in
-test) echo ""
-while true
-do
-clear                     
-                        instalarsi=S
-                        case $instalarsi in
-                        [sS]* ) echo ""
-                        cd /tmp
-                        git clone http://github.com/ea3eiz/test
-                        cd test
-                        sudo sh script_raspberry.sh                  
-                        break;;
-                        [nN]* ) echo ""
-                        clear
-                        exit;
-                        break;;
-esac
-done;;
 a) echo ""
 while true
 do
@@ -385,7 +367,171 @@ echo ""
                     read -p '   QUIERES SEGUIR S/N: ' seguir
                     case $seguir in
                     [sS]* ) echo ""
-                    echo ""
+                    cp /home/pi/.local/Downloads/*.* /home/pi/Downloads
+
+
+                    echo "*********************************************"
+echo "*     SE ESTÁ REALIZANDO LA RESTAURACIÓN    *"
+echo "*********************************************"
+sleep 3
+            cd /home/pi/Downloads
+            cp -f TODOS_LOS_INIS.ini /home/pi/MMDVMHost
+            cp -f MMDVM.ini /home/pi/MMDVMHost
+            cp -f MMDVM.ini_copia /home/pi/MMDVMHost
+            cp -f MMDVM.ini_copia2 /home/pi/MMDVMHost
+            cp -f MMDVM.ini_copia3 /home/pi/MMDVMHost
+            cp -f MMDVM.ini_original /home/pi/MMDVMHost
+            cp -f MMDVMBM.ini /home/pi/MMDVMHost
+            cp -f MMDVMBM.ini_copia /home/pi/MMDVMHost
+            cp -f MMDVMBM.ini_copia2 /home/pi/MMDVMHost
+            cp -f MMDVMBM.ini_copia3 /home/pi/MMDVMHost     
+            cp -f MMDVMPLUS.ini /home/pi/MMDVMHost
+            cp -f MMDVMPLUS.ini_copia /home/pi/MMDVMHost
+            cp -f MMDVMPLUS.ini_copia2 /home/pi/MMDVMHost
+            cp -f MMDVMPLUS.ini_copia3 /home/pi/MMDVMHost
+
+            #solo Dstar y solo Fusion
+            cp -f MMDVMDSTAR.ini /home/pi/MMDVMHost
+            cp -f MMDVMFUSION.ini /home/pi/MMDVMHost
+            #fin  Dstar y solo Fusion
+
+            cp -f YSF2DMR.ini /home/pi/YSF2DMR
+            cp -f YSF2DMR.ini_copia_01 /home/pi/YSF2DMR
+            cp -f YSF2DMR.ini_copia_02 /home/pi/YSF2DMR
+            cp -f YSF2DMR.ini_copia_03 /home/pi/YSF2DMR
+            cp -f YSF2DMR.ini_copia_04 /home/pi/YSF2DMR
+            
+            cp -f TG-YSFList.txt /home/pi/DMR2YSF
+
+            cp -f MMDVMDMR2NXDN.ini /home/pi/MMDVMHost
+            cp -f MMDVMNXDN.ini /home/pi/MMDVMHost
+            cp -f MMDVMDMR2YSF.ini /home/pi/MMDVMHost
+
+            cp -f DMR2NXDN.ini  /home/pi/DMR2NXDN
+            cp -f NXDNGateway.ini /home/pi/NXDNClients/NXDNGateway
+            cp -f DMR2YSF.ini /home/pi/DMR2YSF
+
+            sudo cp -f YSFGateway.ini /home/pi/YSFClients/YSFGateway/
+
+            sudo cp -f BlueDVconfig.ini /home/pi/bluedv/
+
+            sudo cp -f svxlink.conf /usr/local/etc/svxlink/
+            
+            sudo cp -f ModuleEchoLink.conf /usr/local/etc/svxlink/svxlink.d/
+
+            cp ircddbgateway /usr/local/etc/opendv/
+            cp dstarrepeater /usr/local/etc/opendv/
+
+            cp info_panel_control.ini /home/pi/
+
+# Restaura todos los datos de Dvswitch
+indicativo=$(awk "NR==1" /home/pi/Downloads/datos_dvswitch)          
+sudo sed -i "2c Callsign=$indicativo" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "2c Callsign=$indicativo" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "2c Callsign=$indicativo" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "2c Callsign=$indicativo" /opt/MMDVM_Bridge/especial.ini
+sudo sed -i "2c Callsign=$indicativo" /opt/NXDNGateway.ini
+sudo sed -i "2c gatewayCallsign=$indicativo" /etc/ircddbgateway
+sudo sed -i "95c ircddbUsername=$indicativo" /etc/ircddbgateway
+sudo sed -i "117c dplusLogin=$indicativo" /etc/ircddbgateway
+sudo sed -i "2c Callsign=$indicativo" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+id=$(awk "NR==3" /home/pi/Downloads/datos_dvswitch)
+id_nxdn=`expr substr $id 3 5`
+sudo sed -i "30c ID = $id" /opt/MMDVM_Bridge/DVSwitch.ini
+sudo sed -i "40c FallbackID = $id" /opt/MMDVM_Bridge/DVSwitch.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/Analog_Bridge.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/dmr.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/dstar.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/especial.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "43c ;; FallbackID = $id_nxdn" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "44c ;; NXDNFallbackID = $id_nxdn" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/ysf.ini
+sudo sed -i "43c ;; FallbackID = $id" /opt/Analog_Bridge/ysf.ini
+sudo sed -i "38c gatewayDmrId = $id" /opt/Analog_Bridge/FCS.ini
+
+id2=$(awk "NR==4" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "3c Id=$id2" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "3c Id=$id2" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "3c Id=$id2" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "3c Id=$id2" /opt/MMDVM_Bridge/especial.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/Analog_Bridge.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/dmr.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/dstar.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/especial.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/ysf.ini
+sudo sed -i "39c repeaterID = $id2" /opt/Analog_Bridge/FCS.ini
+sudo sed -i "3c Id=$id2" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+Latitude=$(awk "NR==5" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "11c Latitude=$Latitude" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "11c Latitude=$Latitude" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "11c Latitude=$Latitude" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "11c Latitude=$Latitude" /opt/MMDVM_Bridge/especial.ini
+sudo sed -i "11c Latitude=$Latitude" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+Longitude=$(awk "NR==6" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "12c Longitude=$Longitude" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "12c Longitude=$Longitude" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "12c Longitude=$Longitude" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "12c Longitude=$Longitude" /opt/MMDVM_Bridge/especial.ini
+sudo sed -i "12c Longitude=$Longitude" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+port=$(awk "NR==7" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/Analog_Bridge.ini
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/dmr.ini
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/dstar.ini
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/especial.ini
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "55c txPort = $port" /opt/Analog_Bridge/ysf.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/Analog_Bridge.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/dmr.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/dstar.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/especial.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/nxdn.ini
+sudo sed -i "56c rxPort = $port" /opt/Analog_Bridge/ysf.ini
+
+
+location=$(awk "NR==8" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "14c $location" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "14c $location" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "14c $location" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "14c $location" /opt/MMDVM_Bridge/especial.ini
+#sudo sed -i "17c $location" /opt/NXDNGateway.ini
+sudo sed -i "14c $location" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+url=$(awk "NR==9" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "16c URL=$url" /opt/MMDVM_Bridge/MMDVM_Bridge.ini
+sudo sed -i "16c URL=$url" /opt/MMDVM_Bridge/brandmeister_esp.ini
+sudo sed -i "16c URL=$url" /opt/MMDVM_Bridge/dmrplus.ini
+sudo sed -i "16c URL=$url" /opt/MMDVM_Bridge/especial.ini
+sudo sed -i "16c URL=$url" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
+
+address_especial=$(awk "NR==2" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "70c $address_especial" /opt/MMDVM_Bridge/especial.ini
+
+password_especial=$(awk "NR==10" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "74c $password_especial" /opt/MMDVM_Bridge/especial.ini
+
+port_especial=$(awk "NR==11" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "71c $port_especial" /opt/MMDVM_Bridge/especial.ini
+
+sala_fcs=$(awk "NR==12" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "40c txTg = $sala_fcs" /opt/Analog_Bridge/FCS.ini
+sudo sed -i "39c Startup=$sala_fcs" /home/pi/YSFClients/YSFGateway/YSFGateway.ini
+
+
+sala_nxdn=$(awk "NR==13" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "10c $sala_nxdn" /opt/NXDNClients/NXDNGateway/private/NXDNHosts.txt
+
+selfcare=$(awk "NR==14" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "74c $selfcare" /opt/MMDVM_Bridge/brandmeister_esp.ini
+
+reflector_dstar=$(awk "NR==15" /home/pi/Downloads/datos_dvswitch)
+sudo sed -i "18c $reflector_dstar" /etc/ircddbgateway
+                    
                     break;;
                     [nN]* ) 
                     echo ""
