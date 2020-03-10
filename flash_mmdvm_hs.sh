61bfc3487e32906a884f41e84a233b6f96b8f1e6
Manel <ea3eiz@ea3eiz.com>
2020-03-10 11:00:59 +0100

    Update flash_mmdvm_hs.sh

--- flash_mmdvm_hs.sh
+++ flash_mmdvm_hs.sh
@@ -28,7 +28,7 @@ echo -n "${BLANCO}   "
 tac $(ls -1rt /home/pi/MMDVMHost/MMDVM-*.log | tail -n1 ) | grep "protocol" -m 1 | sed -n 's/description: /&\n/;s/.*\n//p'
 #echo "La versión se actualiza al abrir de nuevo MMMDVMHost"
 echo ""
-echo "\33[1;36m   0)\33[1;34m Salir del script \33[1;31m OJO!! no salir con ctrl+c ni con la x"
+echo "   ${ROJO}0) Salir"
 echo ""
 echo -n "\33[1;36m   Elige una opción: " 
 read escoger_menu
