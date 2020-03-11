cb19939debb96e183543857a76d5b23aaa96926f
Manel <ea3eiz@ea3eiz.com>
2020-03-11 09:36:53 +0100

    Update copiar_restaurar.sh

--- copiar_restaurar.sh
+++ copiar_restaurar.sh
@@ -190,7 +190,7 @@ echo "*********************************************"
 echo "*     SE ESTÁ REALIZANDO LA RESTAURACIÓN    *"
 echo "*********************************************"
 sleep 3
-            cd /home/pi/Downloads
+            cd /home/pi/.local/COPIA_SEGURIDAD/Downloads
             cp -f TODOS_LOS_INIS.ini /home/pi/MMDVMHost
             cp -f MMDVM.ini /home/pi/MMDVMHost
             cp -f MMDVM.ini_copia /home/pi/MMDVMHost
