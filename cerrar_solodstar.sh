0d7e910ca8dd85477c67f0321c7966930c1a0477
Manel <ea3eiz@ea3eiz.com>
2020-03-12 10:40:10 +0100

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -20,7 +20,9 @@ sleep 2
 sudo rm /home/pi/Abrir_solodstar.desktop
 
 
-
+cd /home/pi/Desktop
+sudo cp Abrir_ircDDB.desktop /home/pi
+sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x8 -e sudo sh ejecutar_ircDDB.sh; sudo ircddbgateway -gui'" /home/pi/Abrir_ircDDB.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDB.desktop
 sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDB.desktop
