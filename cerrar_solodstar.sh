8c203c6d148957452bdd9a99be9fa2ebbf1d8ad3
Manel <ea3eiz@ea3eiz.com>
2020-03-12 10:56:01 +0100

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -11,7 +11,7 @@ sleep 1
 
 sed -i "6c Exec=sh -c 'cd /home/pi/V104/; sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/Abrir_solodstar.desktop
-sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/pi/Abrir_solodstar.desktop
+sed -i "11c Name[es_ES]=xAbrir solo D-STAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c OLODSTAR=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi
@@ -25,7 +25,7 @@ sudo cp Abrir_ircDDB.desktop /home/pi
 sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x8 -e sudo sh ejecutar_ircDDB.sh; sudo ircddbgateway -gui'" /home/pi/Abrir_ircDDB.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDB.desktop
-sed -i "10c Name[es_ES]=xAbrir ircDDB" /home/pi/Abrir_ircDDB.desktop
+sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDB.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
 sleep 1
 cd /home/pi
