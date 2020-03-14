b68e759adac1f7314f1a56a103bfe849c4087127
Manel <ea3eiz@ea3eiz.com>
2020-03-14 19:41:49 +0100

    Update menu_extra.sh

--- menu_extra.sh
+++ menu_extra.sh
@@ -97,35 +97,35 @@ clear
                         
                         sudo sed -i "10c iface eth0 inet static" /etc/network/interfaces
                         
-                        echo -n "Introduce la ip que quires fijar: ${AMARILLO}Ejp.192.168.1.15 ${BLANCO}"
+                        echo -n "   Introduce la ip que quires fijar: ${AMARILLO}Ejp.192.168.1.15 ${BLANCO}"
                         read ip
                         sudo sed -i "11c address $ip" /etc/network/interfaces
 
                         sudo sed -i "12c netmask 255.255.255.0" /etc/network/interfaces
                         
                         echo "${CIAN}"
-                        echo "Introduce network"
-                        echo "================="
-                        echo "${CIAN}si tu rango es 192.168.1.1 , ${AMARILLO}introduce 192.168.1.0 "
-                        echo "${CIAN}si tu rango es 192.168.0.1 , ${AMARILLO}introduce 192.168.0.0 "
-                        echo "${CIAN}si tu rango no es ninguno de los 2, pon tu rango sustituyendo"
-                        echo -n "las xxx y el último digito por un (0) ${AMARILLO} introduce xxx.xxx.x.0 ${BLANCO}"                        
+                        echo "   Introduce network"
+                        echo "   ================="
+                        echo "${CIAN}   si tu rango es 192.168.1.1 , ${AMARILLO}introduce 192.168.1.0 "
+                        echo "${CIAN}   si tu rango es 192.168.0.1 , ${AMARILLO}introduce 192.168.0.0 "
+                        echo "${CIAN}   si tu rango no es ninguno de los 2, pon tu rango sustituyendo"
+                        echo -n "   las xxx y el último digito por un (0) ${AMARILLO} introduce xxx.xxx.x.0 ${BLANCO}"                        
                         read network
                         sudo sed -i "13c network $network" /etc/network/interfaces
 
                         echo ""
                                                 echo "${CIAN}"
-                        echo "Broadcast"
-                        echo "================="
-                        echo "${CIAN}si tu rango es 192.168.1.1 , ${AMARILLO}introduce 192.168.1.255 "
-                        echo "${CIAN}si tu rango es 192.168.0.1 , ${AMARILLO}introduce 192.168.0.255 "
-                        echo "${CIAN}si tu rango no es ninguno de los 2, pon tu rango sustituyendo"
-                        echo -n "las xxx y el último digito por (255) ${AMARILLO} introduce xxx.xxx.x.255 ${BLANCO}"                        
+                        echo "   Broadcast"
+                        echo "   ================="
+                        echo "${CIAN}   si tu rango es 192.168.1.1 , ${AMARILLO}introduce 192.168.1.255 "
+                        echo "${CIAN}   si tu rango es 192.168.0.1 , ${AMARILLO}introduce 192.168.0.255 "
+                        echo "${CIAN}   si tu rango no es ninguno de los 2, pon tu rango sustituyendo"
+                        echo -n "   las xxx y el último digito por (255) ${AMARILLO} introduce xxx.xxx.x.255 ${BLANCO}"                        
                         read broadcast
                         sudo sed -i "14c broadcast $broadcast" /etc/network/interfaces
 
                         echo "${CIAN}"
-                        echo -n "Introduce la puerta de enlace de tu router: ${AMARILLO}Ejp.192.168.1.1 ${BLANCO}"
+                        echo -n "   Introduce la puerta de enlace de tu router: ${AMARILLO}Ejp.192.168.1.1 ${BLANCO}"
                         read gateway
                         sudo sed -i "15c gateway $gateway" /etc/network/interfaces
                         
