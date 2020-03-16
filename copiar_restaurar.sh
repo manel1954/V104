b5d5aa88e86a462d8a1174582e1b9e2c97bda0fe
Manel <ea3eiz@ea3eiz.com>
2020-03-17 00:37:36 +0100

    Update copiar_restaurar.sh

--- copiar_restaurar.sh
+++ copiar_restaurar.sh
@@ -56,14 +56,22 @@ clear
             cp -f MMDVM.ini_copia2 /home/pi/Downloads
             cp -f MMDVM.ini_copia3 /home/pi/Downloads
             cp -f MMDVM.ini_original /home/pi/Downloads
+
             cp -f MMDVMBM.ini /home/pi/Downloads
             cp -f MMDVMBM.ini_copia /home/pi/Downloads
             cp -f MMDVMBM.ini_copia2 /home/pi/Downloads
-            cp -f MMDVMBM.ini_copia3 /home/pi/Downloads      
+            cp -f MMDVMBM.ini_copia3 /home/pi/Downloads
+
             cp -f MMDVMPLUS.ini /home/pi/Downloads
             cp -f MMDVMPLUS.ini_copia /home/pi/Downloads
             cp -f MMDVMPLUS.ini_copia2 /home/pi/Downloads
             cp -f MMDVMPLUS.ini_copia3 /home/pi/Downloads
+
+            cp -f MMDVMESPECIAL.ini /home/pi/Downloads
+            cp -f MMDVMESPECIAL.ini_copia /home/pi/Downloads
+            cp -f MMDVMESPECIAL.ini_copia2 /home/pi/Downloads
+            cp -f MMDVMESPECIAL.ini_copia3 /home/pi/Downloads
+
             cp -f MMDVMDMR2NXDN.ini /home/pi/Downloads
             cp -f MMDVMDMR2YSF.ini /home/pi/Downloads
             cp -f MMDVMNXDN.ini /home/pi/Downloads
@@ -197,19 +205,27 @@ echo "*********************************************"
 sleep 3
             cd /home/pi/.local/COPIA_SEGURIDAD/Downloads
             cp -f TODOS_LOS_INIS.ini /home/pi/MMDVMHost
+
             cp -f MMDVM.ini /home/pi/MMDVMHost
             cp -f MMDVM.ini_copia /home/pi/MMDVMHost
             cp -f MMDVM.ini_copia2 /home/pi/MMDVMHost
             cp -f MMDVM.ini_copia3 /home/pi/MMDVMHost
             cp -f MMDVM.ini_original /home/pi/MMDVMHost
+
             cp -f MMDVMBM.ini /home/pi/MMDVMHost
             cp -f MMDVMBM.ini_copia /home/pi/MMDVMHost
             cp -f MMDVMBM.ini_copia2 /home/pi/MMDVMHost
-            cp -f MMDVMBM.ini_copia3 /home/pi/MMDVMHost     
+            cp -f MMDVMBM.ini_copia3 /home/pi/MMDVMHost 
+
             cp -f MMDVMPLUS.ini /home/pi/MMDVMHost
             cp -f MMDVMPLUS.ini_copia /home/pi/MMDVMHost
             cp -f MMDVMPLUS.ini_copia2 /home/pi/MMDVMHost
             cp -f MMDVMPLUS.ini_copia3 /home/pi/MMDVMHost
+
+            cp -f MMDVMESPECIAL.ini /home/pi/MMDVMHost
+            cp -f MMDVMESPECIAL.ini_copia /home/pi/MMDVMHost
+            cp -f MMDVMESPECIAL.ini_copia2 /home/pi/MMDVMHost
+            cp -f MMDVMESPECIAL.ini_copia3 /home/pi/MMDVMHost
 
             #solo Dstar y solo Fusion
             cp -f MMDVMDSTAR.ini /home/pi/MMDVMHost
