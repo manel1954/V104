6fb5c44c908f3488d0f5718cd2a8638bbab4ea5e
Manel <ea3eiz@ea3eiz.com>
2020-03-17 16:59:14 +0100

    Update ejecutar_ambe_server.sh

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -28,6 +28,9 @@ sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
-				
+cd /home/pi
+sudo cp Abrir_ambe_server.desktop /home/pi/Desktop
+sleep 2
+sudo rm /home/pi/Abrir_ambe_server.desktop				
 						
 						
