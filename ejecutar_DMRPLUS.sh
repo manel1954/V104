776c9e58867ca3bb08dd04ea4890ac5562a6bc80
Manel <ea3eiz@ea3eiz.com>
2020-03-17 17:08:28 +0100

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -27,7 +27,7 @@ sed -i "2c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
-sudo sed -i "4c Exec=sh 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal -e sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
