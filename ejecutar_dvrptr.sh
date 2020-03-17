015a377cc0d771cd2aa9fd67cf135df6b122c7f8
Manel <ea3eiz@ea3eiz.com>
2020-03-17 18:38:19 +0100

    Update ejecutar_dvrptr.sh

--- ejecutar_dvrptr.sh
+++ ejecutar_dvrptr.sh
@@ -6,6 +6,8 @@ cp Abrir_Dvrptr.desktop /home/pi
 sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dvrptr.sh'" /home/pi/Abrir_Dvrptr.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICONO_DVRPTR_ON.png" /home/pi/Abrir_Dvrptr.desktop
 sed -i "11c Name[es_ES]=cerrar Dvrptr" /home/pi/Abrir_Dvrptr.desktop
+sed -i "20c DVRPTR=ON" /home/pi/status.ini
+
 cd /home/pi
 cp Abrir_Dvrptr.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_Dvrptr.desktop
