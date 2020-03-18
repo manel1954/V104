855238373d71a04400afcdb633155224e7d16c3e
Manel <ea3eiz@ea3eiz.com>
2020-03-18 17:36:11 +0100

    Update ejecutar_radio.sh

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -45,7 +45,7 @@ sudo ./MMDVMHost MMDVM.ini
 
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/MMDVM.png" /home/pi/Abrir_Radio.desktop
 sed -i "10c Name[es_ES]=Abrir Radio" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
