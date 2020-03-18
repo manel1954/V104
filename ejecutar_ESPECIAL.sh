50fc5d4ac54b94a3b53ee84fdb61eb3434a627fa
Manel <ea3eiz@ea3eiz.com>
2020-03-18 15:37:49 +0100

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -25,9 +25,9 @@ frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMESPECIAL.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
 sed -i "11c Name=$frecuencia" /home/pi/RXF_ESPECIAL.desktop
-sleep 1
+#sleep 1
 sudo cp /home/pi/RXF_ESPECIAL.desktop /home/pi/Desktop
-sleep 1s
+#sleep 1
 sudo rm /home/pi/RXF_ESPECIAL.desktop
 
 #Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                        
@@ -43,7 +43,7 @@ sed -i "10c MMDVMESPECIAL=ON" /home/pi/status.ini
 
 cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
-sleep 1
+#sleep 1
 sudo rm /home/pi/Abrir_ESPECIAL.desktop
 
 cd /home/pi/MMDVMHost
@@ -52,7 +52,7 @@ clear
 echo "***********************************************"
 echo "*              ABRIENDO ESPECIAL              * "
 echo "***********************************************"
-sleep 1
+#sleep 1
 sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
 
 cd /home/pi/Desktop
