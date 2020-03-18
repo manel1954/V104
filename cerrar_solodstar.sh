587df402b4b1c43285b3144d08dee494f82281ad
Manel <ea3eiz@ea3eiz.com>
2020-03-19 00:51:18 +0100

    Update cerrar_solodstar.sh

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -6,12 +6,12 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
-sleep 1
+
 sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/; sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo DSTAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
-sleep 1
+
 cd /home/pi
 sudo cp Abrir_solodstar.desktop /home/pi/Desktop
 
@@ -20,12 +20,12 @@ sudo rm /home/pi/Abrir_solodstar.desktop
 
 cd /home/pi/Desktop
 sudo cp Abrir_ircDDB.desktop /home/pi
-sleep 1
+
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_ircDDB.sh'" /home/pi/Abrir_ircDDB.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_OFF.png" /home/pi/Abrir_ircDDB.desktop
 sed -i "10c Name[es_ES]=Abrir ircDDB" /home/pi/Abrir_ircDDB.desktop
 sed -i "1c D-STAR=OFF" /home/pi/status.ini
-sleep 1
+
 cd /home/pi
 sudo cp Abrir_ircDDB.desktop /home/pi/Desktop
 
