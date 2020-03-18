fdb0f4cd0490d9580df844e0647082befbacbe2a
Manel <ea3eiz@ea3eiz.com>
2020-03-18 19:32:14 +0100

    Update ejecutar_YSF2DMR.sh

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -35,14 +35,14 @@ sudo cp Abrir_YSF2DMR.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_YSF2DMR.desktop
 
 cd /home/pi/YSF2DMR
-sudo lxterminal --geometry=80x15 -e ./YSF2DMR YSF2DMR.ini & 
+sudo ./YSF2DMR YSF2DMR.ini & 
 sleep 2
 cd /home/pi/MMDVMHost
-sudo lxterminal --geometry=80x15 -e ./MMDVMFUSION MMDVMFUSION.ini
+sudo ./MMDVMFUSION MMDVMFUSION.ini
 
 cd /home/pi/Desktop
 sudo cp Abrir_YSF2DMR.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x20 -e sudo sh ejecutar_YSF2DMR.sh'" /home/pi/Abrir_YSF2DMR.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x15 -e sudo sh ejecutar_YSF2DMR.sh'" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_YSF2DMR_OFF.png" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "10c Name[es_ES]=Abrir YSF2DMR" /home/pi/Abrir_YSF2DMR.desktop
 sed -i "14c YSF2DMR=OFF" /home/pi/status.ini
