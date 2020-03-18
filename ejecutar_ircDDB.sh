37536f656e6e8d09290f34fdc5bf6a611631e6a1
Manel <ea3eiz@ea3eiz.com>
2020-03-18 21:39:03 +0100

    Update ejecutar_ircDDB.sh

--- ejecutar_ircDDB.sh
+++ ejecutar_ircDDB.sh
@@ -54,7 +54,16 @@ echo " *************************************************************************
 echo " *************************   ABRIENDO ircDDBGateway    ************************"
 echo " ******************************************************************************"
 sleep 2	
-
+cd /home/pi/Desktop
+sudo cp Abrir_ircDDB.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_ircDDB.sh'" /home/pi/Abrir_ircDDB.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_IRCDDB_ON.png" /home/pi/Abrir_ircDDB.desktop
+sed -i "10c Name[es_ES]=Cerrar ircDDB" /home/pi/Abrir_ircDDB.desktop
+sed -i "1c D-STAR=ON" /home/pi/status.ini
+cd /home/pi
+sudo cp Abrir_ircDDB.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_ircDDB.desktop
 fi
  
 
