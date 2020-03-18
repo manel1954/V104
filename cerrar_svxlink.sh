041f620397f07840022a1a6b0ff25736c76a3792
Manel <ea3eiz@ea3eiz.com>
2020-03-18 19:53:57 +0100

    Update cerrar_svxlink.sh

--- cerrar_svxlink.sh
+++ cerrar_svxlink.sh
@@ -4,7 +4,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
 sleep 1
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ECHOLINK.png" /home/pi/Abrir_svxlink.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
 sleep 1
