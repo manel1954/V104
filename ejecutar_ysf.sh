39285a84d447443818779cbf428584869175e772
Manel <ea3eiz@ea3eiz.com>
2020-03-18 19:13:33 +0100

    Update ejecutar_ysf.sh

--- ejecutar_ysf.sh
+++ ejecutar_ysf.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+
+#Colores 
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
+
 cd /home/pi/Desktop
 sudo cp Abrir_YSF.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_ysf.sh'" /home/pi/Abrir_YSF.desktop
@@ -12,7 +22,7 @@ sudo cp Abrir_YSF.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_YSF.desktop
 
 cd /home/pi/YSFClients/YSFGateway
-echo "${VERDE}"
+echo "${MARRON}"
 echo " ******************************************************************************"
 echo " ******************************   ABRIENDO YSF    *****************************"
 echo " ******************************************************************************"
