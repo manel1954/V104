1aac4792eca3cf70934be509c59611c5eb6edcd3
Manel <ea3eiz@ea3eiz.com>
2020-03-18 18:43:18 +0100

    1

--- cerrar_solofusion.sh
+++ cerrar_solofusion.sh
@@ -5,7 +5,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_solofusion.desktop /home/pi
 
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x20 -e sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;lxterminal --geometry=80x15 -e sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF.png" /home/pi/Abrir_solofusion.desktop
 sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
 sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
