40263cc39c5313d8f08a2ef3d670142c4a0177dd
Manel <ea3eiz@ea3eiz.com>
2020-03-18 19:56:08 +0100

    1

--- cerrar_svxlink.sh
+++ cerrar_svxlink.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+sudo killall svxlink
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sleep 1
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
-sleep 1
+
 cd /home/pi
 sudo cp Abrir_svxlink.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/Abrir_svxlink.desktop
 
-sudo killall svxlink
+
 
--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -2,24 +2,24 @@
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_ON.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Cerrar SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_svxlink.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/Abrir_svxlink.desktop
 
 sudo svxlink
 
 cd /home/pi/Desktop
 sudo cp Abrir_svxlink.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh cerrar_svxlink.sh'" /home/pi/Abrir_svxlink.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_SVXLINK_OFF.png" /home/pi/Abrir_svxlink.desktop
 sed -i "10c Name[es_ES]=Abrir SVXLINK" /home/pi/Abrir_svxlink.desktop
 sed -i "8c SVXLINK=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_svxlink.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/Abrir_svxlink.desktop
