f150374c6e19e6beeddfe67d5282cb47f4e5fc5f
Manel <ea3eiz@ea3eiz.com>
2020-03-18 17:24:40 +0100

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "***********************************************"
-echo "*            ABRIENDO BRANDMEISTER            * "
-echo "***********************************************"
 mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMBM.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
@@ -12,14 +9,14 @@ puerto=`expr substr $mode 11 9`
 puerto="  "$puerto
 cd /home/pi/Desktop
 sudo cp RXF_BM.desktop /home/pi
-sleep 1
+
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
 sed -i "11c Name=$frecuencia" /home/pi/RXF_BM.desktop
-sleep 1
+
 sudo cp /home/pi/RXF_BM.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/RXF_BM.desktop
 
 #Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                        
@@ -28,22 +25,21 @@ sed -i "1c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
-sleep 1
+
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
 
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
-clear
-echo "***********************************************"
-echo "*            ABRIENDO BRANDMEISTER            * "
-echo "***********************************************"
-sleep 1
+echo " ******************************************************************************"
+echo " ************************    ABRIENDO BRANDMEISTER    *************************"
+echo " ******************************************************************************"
+
 sudo ./MMDVMBM MMDVMBM.ini
 
 cd /home/pi/Desktop
