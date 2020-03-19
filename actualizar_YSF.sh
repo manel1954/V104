ff02261826f1e41a612e35a338114245cb8e02ad
Manel <ea3eiz@ea3eiz.com>
2020-03-19 14:10:46 +0100

    1

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -7,12 +7,14 @@ clear
 usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
 SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
 
+#Colores 
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
 BLANCO="\033[1;37m"
 AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
+MARRON="\33[38;5;138m"
 echo "${VERDE}"
 echo "   **************************************************************************"
 echo "              Script para actualizar y ver reflectores YSF \33[1;31m by EA3EIZ\33[1;32m   "
