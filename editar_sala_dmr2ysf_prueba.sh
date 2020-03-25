713cba97e7e51ce578e2a683e4e091502dd2303b
Manel <ea3eiz@ea3eiz.com>
2020-03-25 23:53:14 +0100

    Update editar_sala_dmr2ysf_prueba.sh

--- editar_sala_dmr2ysf_prueba.sh
+++ editar_sala_dmr2ysf_prueba.sh
@@ -33,7 +33,7 @@ echo -n "${CIAN} TG: ${AMARILLO} $TG1  ${CIAN} SALA: ${AMARILLO} $SALA1"
 
 
 
-    num_carac= `wc -m SALA1`
+    num_carac= `wc -m $SALA1`
     echo $SALA1 "tiene" $num_carac "caracteres"
 
 read a
