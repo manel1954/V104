72e4a40a2b4775a98f87e8530c76859d08ec6c2d
Manel <ea3eiz@ea3eiz.com>
2020-03-28 00:19:53 +0100

    Update editar_ircddbgateway.sh

--- editar_ircddbgateway.sh
+++ editar_ircddbgateway.sh
@@ -32,7 +32,7 @@ echo "$indicativo"
 echo -n "${CIAN}   2)${GRIS} Modificar Ciudad      - ${AMARILLO}"
 ciudad=`grep "Location=" /usr/local/etc/opendv/ircddbgateway`
 ciudad=`expr substr $ciudad 14 20`
-echo "ciudad"
+echo "$ciudad"
 
 echo ""
 echo "   ${ROJO}0) Volver"
