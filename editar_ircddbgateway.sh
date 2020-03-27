a61a77c5f00c1e3ced37539f831654ec1462b803
Manel <ea3eiz@ea3eiz.com>
2020-03-27 22:47:38 +0100

    Update editar_ircddbgateway.sh

--- editar_ircddbgateway.sh
+++ editar_ircddbgateway.sh
@@ -25,8 +25,8 @@ echo -n "${VERDE}"
 echo "   **************************************************************************"
 
 echo -n "${CIAN}   1)${GRIS} Modificar indicativo  - ${AMARILLO}"
-indicativo= sed -n '2p' /usr/local/etc/opendv/ircddbgateway
-#indicativo=`grep -n "gatewayCallsign=" /usr/local/etc/opendv/ircddbgateway`
+#indicativo= sed -n '2p' /usr/local/etc/opendv/ircddbgateway
+indicativo=`grep -n "gatewayCallsign=" /usr/local/etc/opendv/ircddbgateway`
 indicativo1=`expr substr $indicativo 17 7`
 echo "$indicativo1"
 read a
