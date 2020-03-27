f15d5f628d1eb7fe36a354108648e1461e063623
Manel <ea3eiz@ea3eiz.com>
2020-03-27 08:59:04 +0100

    Update editar_MMDVMBM.sh

--- editar_MMDVMBM.sh
+++ editar_MMDVMBM.sh
@@ -1330,7 +1330,7 @@ do
                           actualizar=S 
                           case $actualizar in
                           [sS]* ) echo ""
-                          cd /home/pi/V104
+                          cd /home/pi/Desktop
                           lxterminal --geometry=80x15 -e screen sudo sh ejecutar_bm.sh & sleep 2 & exit;
                          
                           break;;
