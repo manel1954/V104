b1190ea3dcf35dc95d3ab5be7598acfa4e7b304a
Manel <ea3eiz@ea3eiz.com>
2020-03-29 18:18:49 +0200

    Update actualizar_ircddb_2019.sh

--- actualizar_ircddb_2019.sh
+++ actualizar_ircddb_2019.sh
@@ -1,19 +1,12 @@
 ﻿#!/bin/bash
 
-
-
-
 sudo mv /usr/local/bin/ircddbgateway /usr/local/bin/ircddbgateway_2016
 sudo mv /usr/local/bin/ircddbgatewayconfig /usr/local/bin/ircddbgatewayconfig_2016
 sudo mv /usr/local/bin/remotecontrol /usr/local/bin/remotecontrol_2016
 
-sudo mv /home/pi/V104/ircddbgateway /usr/bin/ircddbgateway
-sudo mv /home/pi/V104/ircddbgatewayconfig /usr/bin/ircddbgatewayconfig
-sudo mv /home/pi/V104/remotecontrol /usr/bin/remotecontrol
-
-#sudo chmod 755 /usr/bin/ircddbgateway
-#sudo chmod 755 /usr/bin/ircddbgatewayconfig
-#sudo chmod 755 /usr/bin/remotecontrol
+sudo cp /home/pi/V104/ircddbgateway /usr/bin/ircddbgateway
+sudo cp /home/pi/V104/ircddbgatewayconfig /usr/bin/ircddbgatewayconfig
+sudo cp /home/pi/V104/remotecontrol /usr/bin/remotecontrol
 
 sudo rm -R /usr/share/opendv
 sudo mkdir /usr/share/opendv
