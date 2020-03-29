ed876c984efd4e5b3a1879445293bf1345ff7ca4
Manel <ea3eiz@ea3eiz.com>
2020-03-29 21:13:09 +0200

    Update actualizar_ircddb_2019.sh

--- actualizar_ircddb_2019.sh
+++ actualizar_ircddb_2019.sh
@@ -1,26 +1,8 @@
 ﻿#!/bin/bash
-# path usuario
-usuario="/home/pi"
-usuario=$(awk "NR==1" $usuario/.config/autostart/usuario)
-SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-echo "${AMARILLO}"
-echo " *********************************************************************"
-echo " *                                                                   *"
-echo " *                      REINICIAR RASPBERRY PI                       *"
-echo " *                                                                   *"
-echo " *********************************************************************"
-echo ""
+
 echo "${CIAN}"
-echo -n " Quieres Reiniciar S/N ? "
-read activar
-case "$activar" in
+read -p 'Quieres actualizar Ircddbgateway a la última versión ? S/N ' actualizar 
+case "$actualizar" in
 [sS]* )
 clear
 echo "actualiza"
