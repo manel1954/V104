35a5ff63ba3b00c473369f5b6f7d048b8bd9d66e
Manel <ea3eiz@ea3eiz.com>
2020-03-29 17:15:18 +0200

    Update actualizar_ircddb_2019.sh

--- actualizar_ircddb_2019.sh
+++ actualizar_ircddb_2019.sh
@@ -8,13 +8,13 @@ wget http://associacioader.com/descargas/ircddbgateway
 wget http://associacioader.com/descargas/ircddbgatewayconfig
 wget http://associacioader.com/descargas/remotecontrol
 
-sudo mv /usr/local/bin/ ircddbgateway ircddbgateway_2016
-sudo mv /usr/local/bin/ ircddbgatewayconfig ircddbgatewayconfig_2016
-sudo mv /usr/local/bin/ remotecontrol remotecontrol_2016
+sudo mv /usr/local/bin/ircddbgateway /usr/local/bin/ircddbgateway_2016
+sudo mv /usr/local/bin/ircddbgatewayconfig /usr/local/bin/ircddbgatewayconfig_2016
+sudo mv /usr/local/bin/remotecontrol /usr/local/bin/remotecontrol_2016
 
-sudo mv /home/pi/.local/ircddb_2019/ ircddbgateway /usr/bin/ ircddbgateway
-sudo mv /home/pi/.local/ircddb_2019/ ircddbgatewayconfig /usr/bin/ ircddbgatewayconfig
-sudo mv /home/pi/.local/ircddb_2019/ remotecontrol /usr/bin/ remotecontrol
+sudo mv /home/pi/.local/ircddb_2019/ircddbgateway /usr/bin/ircddbgateway
+sudo mv /home/pi/.local/ircddb_2019/ircddbgatewayconfig /usr/bin/ircddbgatewayconfig
+sudo mv /home/pi/.local/ircddb_2019/remotecontrol /usr/bin/remotecontrol
 
 sudo chmod 777 /usr/bin/ircddbgateway
 sudo chmod 777 /usr/bin/ircddbgatewayconfig
