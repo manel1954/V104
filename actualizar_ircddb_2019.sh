65425896d70d9890f6d63c0dfad985ee1154a0e9
Manel <ea3eiz@ea3eiz.com>
2020-03-30 00:56:14 +0200

    Update actualizar_ircddb_2019.sh

--- actualizar_ircddb_2019.sh
+++ actualizar_ircddb_2019.sh
@@ -26,21 +26,21 @@
                       	case $actualizar in
 			          	[sS]* ) echo ""
 						
-						# sudo mv /usr/local/bin/ircddbgateway /usr/local/bin/ircddbgateway_2016
-						# sudo mv /usr/local/bin/ircddbgatewayconfig /usr/local/bin/ircddbgatewayconfig_2016
-						# sudo mv /usr/local/bin/remotecontrol /usr/local/bin/remotecontrol_2016
+						sudo mv /usr/local/bin/ircddbgateway /usr/local/bin/ircddbgateway_2016
+						sudo mv /usr/local/bin/ircddbgatewayconfig /usr/local/bin/ircddbgatewayconfig_2016
+						sudo mv /usr/local/bin/remotecontrol /usr/local/bin/remotecontrol_2016
 
-						# sudo cp /home/pi/V104/ircddbgateway /usr/bin/ircddbgateway
-						# sudo cp /home/pi/V104/ircddbgatewayconfig /usr/bin/ircddbgatewayconfig
-						# sudo cp /home/pi/V104/remotecontrol /usr/bin/remotecontrol
+						sudo cp /home/pi/V104/ircddbgateway /usr/bin/ircddbgateway
+						sudo cp /home/pi/V104/ircddbgatewayconfig /usr/bin/ircddbgatewayconfig
+						sudo cp /home/pi/V104/remotecontrol /usr/bin/remotecontrol
 
-						# sudo rm -R /usr/share/opendv
-						# sudo mkdir /usr/share/opendv
-						# sudo cp /usr/local/share/opendv/*.ambe /usr/share/opendv/
-						# sudo cp /usr/share/ircddbgateway/*.* /usr/share/opendv/
+						sudo rm -R /usr/share/opendv
+						sudo mkdir /usr/share/opendv
+						sudo cp /usr/local/share/opendv/*.ambe /usr/share/opendv/
+						sudo cp /usr/share/ircddbgateway/*.* /usr/share/opendv/
 
-						# sudo rm -R /var/log/opendv
-						# sudo mkdir /var/log/opendv
+						sudo rm -R /var/log/opendv
+						sudo mkdir /var/log/opendv
 
 						sudo sed -i "20c 2019" /home/pi/.local/versiones_YSF2DMR_DMR2YSF_DMR2NXDN.ini
 						clear
