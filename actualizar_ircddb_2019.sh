2f5090e53ce3a5461a3cd042e0857b9c921a5b16
Manel <ea3eiz@ea3eiz.com>
2020-03-29 21:11:16 +0200

    Update actualizar_ircddb_2019.sh

--- actualizar_ircddb_2019.sh
+++ actualizar_ircddb_2019.sh
@@ -1,21 +1,36 @@
 ﻿#!/bin/bash
-
-
-
-
+# path usuario
+usuario="/home/pi"
+usuario=$(awk "NR==1" $usuario/.config/autostart/usuario)
+SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+echo "${AMARILLO}"
+echo " *********************************************************************"
+echo " *                                                                   *"
+echo " *                      REINICIAR RASPBERRY PI                       *"
+echo " *                                                                   *"
+echo " *********************************************************************"
+echo ""
+echo "${CIAN}"
 echo -n " Quieres Reiniciar S/N ? "
 read activar
 case "$activar" in
 [sS]* )
-echo "SE ACTUALIZA"
+clear
+echo "actualiza"
 read a
 break;;
 [nN]*) 
-echo "NO SE ACTUALIZA"
+echo "no actualiza"
 read a
 exit ;;
 esac
-
 
 
 
