b2a9f18794f6c2895111f761c36c0dc301e95de7
Manel <ea3eiz@ea3eiz.com>
2020-04-01 12:52:41 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -24,7 +24,11 @@ echo "${CIAN}"
 sleep 2
 
 # 01-04-2019 arregla no escribe port en FCS.ini
-sudo -i sed "6c exec("sudo sed -i '55c txPort = $port' /opt/Analog_Bridge/FCS.ini");" /var/www/html/cambia_configuracion_port.php
+sudo sed -i "6c exec("sudo sed -i '55c txPort = $port' /opt/Analog_Bridge/FCS.ini");" /var/www/html/cambia_configuracion_port.php
+
+# sudo sed -i "2c $ultima" /var/www/html/comprueba_actualizacion.php
+
+
 
 #exec("sudo sed -i '56c rxPort = $port' /opt/Analog_Bridge/FCS.ini");
 
