d45cf8f9dc013de5706f4bb31ae5b7e6402f6e4f
Manel <ea3eiz@ea3eiz.com>
2020-04-01 13:53:47 +0200

    Update cambia_configuracion_port.php

--- cambia_configuracion_port.php
+++ cambia_configuracion_port.php
@@ -19,5 +19,5 @@ exec("sudo sed -i '56c rxPort = $port' /opt/Analog_Bridge/nxdn.ini");
 exec("sudo sed -i '56c rxPort = $port' /opt/Analog_Bridge/ysf.ini");
 
 header("Location: panel_configuracion.php");	
-
+//esto es el cambio
 ?>
