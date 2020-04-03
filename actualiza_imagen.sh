9be8493c4f64deef6e6200d0e00d95fecac3a83a
Manel <ea3eiz@ea3eiz.com>
2020-04-03 18:53:48 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -26,16 +26,6 @@ sleep 2
 # 01-04-2019 arregla no escribe port en FCS.ini
 sudo cp /home/pi/V104/cambia_configuracion_port.php /var/www/html/
 
-# sudo sed -i "2c $ultima" /var/www/html/comprueba_actualizacion.php
-
-
-
-#exec("sudo sed -i '56c rxPort = $port' /opt/Analog_Bridge/FCS.ini");
-
-
-
-
-
 #Actualiza IMAGEN
                         cd $usuario/$SCRIPTS_version
                         git pull
