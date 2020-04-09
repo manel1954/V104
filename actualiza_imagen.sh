a377de5a4852a323b010fa002c198fded908cd1b
Manel <ea3eiz@ea3eiz.com>
2020-04-09 11:55:23 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -23,14 +23,18 @@ echo "${CIAN}"
 
 sleep 2
 
-			# 01-04-2019 arregla no escribe port en FCS.ini
+			# 01-04-2020 arregla no escribe port en FCS.ini
 			sudo cp /home/pi/V104/cambia_configuracion_port.php /var/www/html/
 
-                        # 03-04-2019 añade el options manual para DMR+
+                        # 03-04-2012020 añade el options manual para DMR+
                         sudo cp /home/pi/V104/sistema_plus.php /var/www/html/
                         sudo cp /home/pi/V104/cambia_reflector_dmrplus.php /var/www/html/
                         sudo cp /home/pi/V104/cambia_options_dmrplus.php /var/www/html/
                         sudo cp /home/pi/V104/panel_configuracion.php /var/www/html/
+
+                        # 09-04-2020 areegla linea 
+                        sudo sed -i "39c # Startup=Alabama-Link" /home/pi/YSFClients/YSFGateway/YSFGateway.ini
+                        sudo sed -i "42c Revert=0" /home/pi/YSFClients/YSFGateway/YSFGateway.ini
 
 #Actualiza IMAGEN
                         cd $usuario/$SCRIPTS_version
