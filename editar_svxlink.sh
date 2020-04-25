1e92d7b0abcaef253e175f40b3253272cbbc0777
Manel <ea3eiz@ea3eiz.com>
2020-04-26 00:42:08 +0200

    1

--- editar_svxlink.sh
+++ editar_svxlink.sh
@@ -397,7 +397,7 @@ clear
 			[sS]* ) echo ""
 			echo "Poniendo roger beep >>>>>"
 			sleep 3
-			sudo cp -f roger_beep.wav /usr/local/share/svxlink/sounds/en_US/Core/
+			sudo cp -f /home/pi/V104/roger_beep.wav /usr/local/share/svxlink/sounds/en_US/Core/
 			sed -i '253c playMsg "Core" "roger_beep";' /usr/local/share/svxlink/events.d/Logic.tcl
 			echo ""
 			echo "Ok, se ha ejecutado correctamente"
