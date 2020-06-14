7e76d4e556d502b9850cc18ab36c5c2401e5be98
Manel <ea3eiz@ea3eiz.com>
2020-06-15 00:00:16 +0200

    1

--- cambia_puerto_usrp.sh
+++ cambia_puerto_usrp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
 
-valor=$(awk "NR==40" /home/pi/.local/datos_dvswitch)
+valor=$(awk "NR==3" /opt/Analog_Bridge/analog_bridge.sh)
 
 sed -i "24c mode=\`cat \/tmp\/ABInfo_$valor.json \| python -c \'import json,sys;obj=json.load(sys.stdin); print obj\[\"tlv\"\]\[\"ambe_mode\"\];\'\`" /opt/Analog_Bridge/analog_bridge.sh
